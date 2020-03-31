92e5ed47acc4fc97cad3e878aafabe40676851c4
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-03-31 13:56:03 -0700

    debugging scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   $("form#puzzle").submit(function(event) {
     event.preventDefault();
     var sentence = $("input#input1").val();
-    
+    var sentenceSplit = sentence.split("");
+    console.log(sentenceSplit);
     $("#output").text(sentence);
+  }
+  function myFunc() {
+    if ()
+  }
+
   });
 });
